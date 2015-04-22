4301e0ae6e5a76da1c0d555746213b4c7602b9a8
Leopoldo Lucas Oliveira <leopoldolucass@gmail.com>
2015-04-22 16:54:38 -0300

    Código Corrigido

---
+++ src/imc_lab3/IMC.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imc_lab3;
+
+/**
+ *
+ * @author Leopoldo
+ */
+public class IMC {
+
+    public static void main(String[] args) {
+        //Criação de um objeto ex da classe Ex2
+        IMCImpl ex = new IMCImpl();
+        //posição da tela ao abri-la
+        ex.setLocation(400, 200);
+        //tornando a tela visível
+        ex.setVisible(true);
+
+    }
+
+}
