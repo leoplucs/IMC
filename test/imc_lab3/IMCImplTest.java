18cc84e47592715c5fb405c8f0656fabe2b62707
leoplucs <leop_lucas22@hotmail.com>
2015-04-27 19:12:43 -0300

    criando testes

--- test/imc_lab3/IMCImplTest.java
+++ test/imc_lab3/IMCImplTest.java
@@ -19,9 +19,31 @@ import static org.junit.Assert.*;
 public class IMCImplTest {
 private void testarCalculos(){
 IMCImpl imc = new IMCImpl();
-
+        
+        imc.altura = 1.7;
+        imc.peso
+        imc.calcularImc();
+        assertEquals("24,221453", String.format("%2f", imc.getIMC()));  
 }
 
-private void testarlimpar() {}
+ 
+    @Test
+    public void testeMensagemMulher(){
+        Ex2 ex = new Ex2();
+        ex.altura = 1.7;
+        ex.peso = 95;
+        ex.calcularIMC();
+        ex.setSexo(SexoEnum.MULHER);
+        assertTrue(ex.tratarIMCMulher().contains("CUIDADO!!Voce esta obesa!"));
+    }
+    @Test
+    public void testeMensagemHomem(){
+        Ex2 ex = new Ex2();
+        ex.altura = 1.7;
+        ex.peso = 70;
+        ex.calcularIMC();
+        ex.setSexo(SexoEnum.HOMEM);
+        assertTrue(ex.tratarImcHomem().contains("PARABENS!!Voce esta com o peso ideal!"));
+    }
     
 }
